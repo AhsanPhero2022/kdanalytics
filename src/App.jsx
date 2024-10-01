9981daa19b80a17b1cc591e2fe9ccd960018e006
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-10-01 22:55:55 +0600

    project install done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <Outlet></Outlet>
+    </>
+  );
+}
+
+export default App;
